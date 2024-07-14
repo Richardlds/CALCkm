b66e0919ebe92156646733f8a05604222f7c72c5
Richard <richardlds2005@gmail.com>
2024-07-14 16:36:05 -0300

    a

--- script.js
+++ script.js
@@ -48,7 +48,7 @@ function calcular() {
 
     const taxaVolare = valorTotal * 0.22;
     const valorNegociar = valorTotal - taxaVolare;
-    const levaTraz = (valorTotal + patinsSelecionado) * 1.5 + (patinsSelecionado + saidaValor) - taxaVolare;
+    const levaTraz = (valorTotal + patinsSelecionado) * 1.5 + patinsSelecionado + saidaValor;
 
     document.getElementById('saida').value = `R$ ${saidaValor.toFixed(2)}`;
     document.getElementById('kmsExcedente').value = `${kmsExcedente} KM`;
